bf38a3288d4c0d6b04ec4a5e3e4c3e370ca89bfe
mirzokhid10 <mxudoyberdiyev21@gmail.com>
2025-02-08 01:30:23 +0500

    Flash Sale- Show flash sale products in frontend

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\FlashSale;
+use App\Models\FlashSaleItem;
 use App\Models\Slider;
 use Illuminate\Http\Request;
 
@@ -12,12 +13,12 @@ class HomeController extends Controller
     public function index() {
         $sliders = Slider::where('status',1)->orderBy('serial','asc')->get();
         $flashSaleDate = FlashSale::first();
+        $flashSaleItems = FlashSaleItem::where('show_at_home', 1)->where('status', 1)->get();
         return view('frontend.home.home',
-
-        compact(
-            'sliders',
-            'flashSaleDate'
-        ));
+            compact(
+                'sliders',
+                'flashSaleDate'
+            ));
     }
 
     // public function login()
